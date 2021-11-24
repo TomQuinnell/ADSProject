b65239f06d21158acdcaf1b6ac56c5c1052a0453
tomquin456@gmail.com <tomquin456@gmail.com>
2021-11-24 20:56:31 +0000

    Updated PLT version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ VERSION = "0.1.0"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    "pandas", "numpy", "jupyter", "matplotlib", "PyMySQL", "mlai", "osmnx"
+    "pandas", "numpy", "jupyter", "matplotlib==3.1.3", "PyMySQL", "mlai", "osmnx"
 ]
 
 # What packages are optional?
