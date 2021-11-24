1a846104b1263da80c2704f1cf4ff6d17fe1d7ed
tomquin456@gmail.com <tomquin456@gmail.com>
2021-11-24 20:11:01 +0000

    Postcode and region join function

--- fynesse/access.py
+++ fynesse/access.py
@@ -118,7 +118,7 @@ def upload_postcode_data(conn):
     print()
 
 
-def join_data_for_region_time(conn, postcode_start, date_of_transfer):
+def join_data_for_postcode_time(conn, postcode_start, date_of_transfer):
     """ Select the data for the region with postcode starting with postcode_start,
         join into one table, and store the results in prices_coordinates_data """
     cur = conn.cursor()
@@ -136,6 +136,28 @@ def join_data_for_region_time(conn, postcode_start, date_of_transfer):
                 AND pp_data.date_of_transfer LIKE '{date_of_transfer}%') pp
         INNER JOIN
             (SELECT * FROM postcode_data WHERE postcode_data.postcode LIKE '{postcode_start}%') pc
+        ON pp.postcode = pc.postcode \n;""")
+    conn.commit()
+
+
+def join_data_for_region_time(conn, region, date_of_transfer):
+    """ Select the data for the region with postcode starting with postcode_start,
+        join into one table, and store the results in prices_coordinates_data """
+    cur = conn.cursor()
+    print("Truncating table...")
+    cur.execute("TRUNCATE TABLE prices_coordinates_data")  # clear the data, preserve structure
+    print("Table truncated, now inserting data for ", region, date_of_transfer)
+    cur.execute(f"""
+        INSERT INTO prices_coordinates_data
+            (price, date_of_transfer, postcode, property_type, new_build_flag, tenure_type, locality,
+            town_city, district, county, country, lattitude, longitude, db_id)
+        SELECT price, date_of_transfer, pp.postcode, property_type, new_build_flag, tenure_type, locality,
+            town_city, district, county, country, lattitude, longitude, pp.db_id
+        FROM
+            (SELECT * FROM pp_data WHERE pp_data.district = '{region}%'
+                AND pp_data.date_of_transfer LIKE '{date_of_transfer}%') pp
+        INNER JOIN
+            postcode_data pc
         ON pp.postcode = pc.postcode \n;""")
     conn.commit()
 
